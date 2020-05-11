3e930b19ffd8dcf98a176f3b09adf75e24126cb5
danielspracklin <40771920+danielspracklin@users.noreply.github.com>
2020-05-11 11:26:39 -0400

    rm cruft from makefile

--- rules.mk
+++ rules.mk
@@ -1,10 +1,1 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
-{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red20\green20\blue20;}
-{\*\expandedcolortbl;;\cssrgb\c10196\c10196\c10196;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs30 \cf2 \expnd0\expndtw0\kerning0
-LEADER_ENABLE = yes}
+LEADER_ENABLE = yes
