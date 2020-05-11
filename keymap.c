be3951c214a0a7ab88b6402eace9e13deac42571
danielspracklin <40771920+danielspracklin@users.noreply.github.com>
2020-05-11 12:22:30 -0400

    pushing experimental config and keymap files

---
+++ keymap.c
@@ -0,0 +1,15 @@
+// I still need to set KC_LEAD() somewhere.
+
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+
+    SEQ_ONE_KEY(KC_G, KC_B) {
+      // When I press KC_LEAD and then G and B, this sends "group_by"
+      SEND_STRING("group_by");
+    }
+  }
+}
