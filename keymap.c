8a8e2bdff0f1b029d87d6e19bc8095406031718a
danielspracklin <40771920+danielspracklin@users.noreply.github.com>
2020-05-11 12:24:30 -0400

    use SEQ_TWO_KEYS(), not SEQ_ONE_KEY()

--- keymap.c
+++ keymap.c
@@ -7,7 +7,7 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    SEQ_ONE_KEY(KC_G, KC_B) {
+    SEQ_TWO_KEYS(KC_G, KC_B) {
       // When I press KC_LEAD and then G and B, this sends "group_by"
       SEND_STRING("group_by");
     }
